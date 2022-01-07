2cc5f72b9b75953016cfe562a5abee25e1560fb3
Mauro Alfaro <38593850+mauroalfaro@users.noreply.github.com>
2022-01-07 16:03:23 -0300

    Branch/categories (#1)
    * Categorias Gerbio - first commit
    
    * Fix id de Fuentes
    
    * Agrego proceso al import de Products
    
    Co-authored-by: Mauro Alfaro <mauro@appsky.io>

--- src/main/java/com/palazzisoft/gerbio/integrator/runner/IntegratorRunner.java
+++ src/main/java/com/palazzisoft/gerbio/integrator/runner/IntegratorRunner.java
@@ -8,13 +8,18 @@ import com.palazzisoft.gerbio.integrator.service.anymarket.BrandService;
 import com.palazzisoft.gerbio.integrator.service.anymarket.CategoryService;
 import com.palazzisoft.gerbio.integrator.service.anymarket.OriginService;
 import com.palazzisoft.gerbio.integrator.service.anymarket.ProductService;
+import com.palazzisoft.gerbio.integrator.util.CSVCategoryReader;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Profile("!test")
 @Component
@@ -25,20 +30,30 @@ public class IntegratorRunner implements ApplicationRunner {
     private final CategoryService categoryService;
     private final ProductService productService;
     private final OriginService originService;
+    private final CSVCategoryReader csvCategoryReader;
 
     public IntegratorRunner(final BrandService brandService, final CategoryService categoryService,
-                            final ProductService productService, final OriginService originService) {
+                            final ProductService productService, final OriginService originService) throws IOException {
         this.brandService = brandService;
         this.categoryService = categoryService;
         this.productService = productService;
         this.originService = originService;
+        csvCategoryReader = new CSVCategoryReader();
     }
 
     @Override
-    public void run(ApplicationArguments args) {
+    public void run(ApplicationArguments args) throws IOException {
         log.debug("Initializing Gerbio Integrator App");
 
         log.info("Retrieving from AnyMarket brands, categories and products to be persisted in DB");
+
+        List<String> variosCsv = csvCategoryReader.readVariosCSV();
+        List<String> componentesPCCsv = csvCategoryReader.readComponentesCSV();
+        List<String> impresorasCsv = csvCategoryReader.readImpresorasCSV();
+        List<String> monitoresCsv = csvCategoryReader.readMonitoresCSV();
+        List<String> notebooksCsv = csvCategoryReader.readNotebooksCSV();
+        List<String> servidoresCsv = csvCategoryReader.readServidoresCSV();
+
         originService.storeInitOrigins();
 
         try {
@@ -49,9 +64,30 @@ public class IntegratorRunner implements ApplicationRunner {
             List<AnyCategory> categories = categoryService.getAll();
             categoryService.saveCategories(categories);
             categoryService.synchronizeCategories();
+            categoryService.storeGerbioCategories();
+
+            List<AnyCategory> categoriesAfterSync = categoryService.findAll();
 
             List<AnyProduct> products = productService.getAll();
+
+            for (AnyProduct product : products) {
+                if (variosCsv.contains(product.getCategory().getId().toString())) {
+                    product.setCategory(findCategoryByPartnerId(categoriesAfterSync, "VS").get());
+                } else if (componentesPCCsv.contains(product.getCategory().getId().toString())) {
+                    product.setCategory(findCategoryByPartnerId(categoriesAfterSync, "CAPC").get());
+                } else if (impresorasCsv.contains(product.getCategory().getId().toString())) {
+                    product.setCategory(findCategoryByPartnerId(categoriesAfterSync, "IPS").get());
+                } else if (servidoresCsv.contains(product.getCategory().getId().toString())) {
+                    product.setCategory(findCategoryByPartnerId(categoriesAfterSync, "SCS").get());
+                } else if (notebooksCsv.contains(product.getCategory().getId().toString())) {
+                    product.setCategory(findCategoryByPartnerId(categoriesAfterSync, "NPAT").get());
+                } else if (monitoresCsv.contains(product.getCategory().getId().toString())) {
+                    product.setCategory(findCategoryByPartnerId(categoriesAfterSync, "MT").get());
+                }
+            }
+
             productService.saveProducts(products);
+
         }
         catch (GerbioException e ) {
             log.error("Error synchronizing data", e);
@@ -61,5 +97,9 @@ public class IntegratorRunner implements ApplicationRunner {
         }
 
         log.info("Synchronization fully performed");
+    }
+
+    private Optional<AnyCategory> findCategoryByPartnerId(List<AnyCategory> category, String partnerId) {
+        return category.stream().filter(c -> c.getPartnerId().equals(partnerId)).findFirst();
     }
 }
